78962001f3581a8656c802f377aee3f700b2074a
SiddharthCh7 <siddharth070504@gmail.com>
2025-11-06 02:43:58 +0530

    updated server url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const axiosInstance = axios.create({
-	baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+	baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : process.env.SERVER_URL,
 });
